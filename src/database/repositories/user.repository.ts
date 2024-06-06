b24196db11e99309393956417cf808e4f6dc62fc
davidguri <david.guri@icloud.com>
2024-06-06 13:13:10 +0200

    restructred repositories and controllers with custom functions

--- src/database/repositories/user.repository.ts
+++ src/database/repositories/user.repository.ts
@@ -1,24 +1,5 @@
 import { dataSource } from "../../typeorm.config";
-// import { Repository } from 'typeorm';
 import { UserEntity } from "../entities/user.entity";
-
-// export class UserRepository extends Repository<UserEntity> {
-//   findOneById(id: string): Promise<UserEntity | null> {
-//     return this.findOne({ where: { id } });
-//   }
-
-//   findAll(): Promise<UserEntity[]> {
-//     return this.find();
-//   }
-
-//   saveObject(obj: UserEntity): Promise<UserEntity | undefined> {
-//     return this.save(obj);
-//   }
-
-//   removeObject(obj: UserEntity): Promise<UserEntity | undefined> {
-//     return this.remove(obj);
-//   }
-// }
 
 export const UserRepository = dataSource.getRepository(UserEntity).extend({
   findAll(): Promise<UserEntity[]> {

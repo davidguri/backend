a932da58e309add9302607b68948477487f25879
davidguri <david.guri@icloud.com>
2024-06-11 15:28:35 +0200

    fixed issue with mapper

--- src/database/mappings/grade.mapper.ts
+++ src/database/mappings/grade.mapper.ts
@@ -1,13 +1,17 @@
 import Grade from "../../models/grade.model";
 import { GradeEntity } from "../entities/grade.entity";
+import { ClassEntity } from "../entities/class.entity";
+import { UserEntity } from "../entities/user.entity";
 
 export class GradeMapper {
-  static toEntity(model: Grade): GradeEntity {
+  static toEntity(model: Grade, classObj: ClassEntity, user: UserEntity): GradeEntity {
     const grade = new GradeEntity();
 
     grade.id = model.id;
     grade.grade = model.grade;
     grade.percentage = model.percentage;
+    grade.class = classObj;
+    grade.user = user;
     grade.createdAt = model.createdAt;
     grade.updatedAt = model.updatedAt;
 
